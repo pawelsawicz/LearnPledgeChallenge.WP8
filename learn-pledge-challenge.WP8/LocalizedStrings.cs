87488928063eda86500157f81c527431dd19be4c
pawelsawicz <pawel.sawicz@justgiving.com>
2014-09-13 00:21:17 +0100

    init project

---
+++ learn-pledge-challenge.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using learn_pledge_challenge.WP8.Resources;
+
+namespace learn_pledge_challenge.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
