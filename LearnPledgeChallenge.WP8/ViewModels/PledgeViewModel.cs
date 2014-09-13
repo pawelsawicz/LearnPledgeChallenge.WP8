10d487636aad28eed47baa3d54adb892256b3091
pawelsawicz <pawel.sawicz@justgiving.com>
2014-09-14 00:13:28 +0100

    pledge view and viewmodel updated

--- LearnPledgeChallenge.WP8/ViewModels/PledgeViewModel.cs
+++ LearnPledgeChallenge.WP8/ViewModels/PledgeViewModel.cs
@@ -12,9 +12,59 @@ namespace LearnPledgeChallenge.WP8.ViewModels
     {
         private readonly INavigationService _navigationService;
 
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                NotifyOfPropertyChange(() => Email);
+            }
+        }
+
+        private string _pledge;
+        public string Pledge
+        {
+            get { return _pledge; }
+            set
+            {
+                _pledge = value;
+                NotifyOfPropertyChange(() => Pledge);
+            }
+        }
+
+        private string _forfeit;
+        public string Forfeit
+        {
+            get { return _forfeit; }
+            set
+            {
+                _forfeit = value;
+                NotifyOfPropertyChange(() => Forfeit);
+            }
+        }
+
+
         public PledgeViewModel(INavigationService navigationService)
         {
-            _navigationService = navigationService; 
+            _navigationService = navigationService;
+        }
+
+        public void TryPledge()
+        {
+            MessageBox.Show("Try Pledge");
         }
     }
 }
