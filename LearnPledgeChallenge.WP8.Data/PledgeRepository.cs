63709e4a8b21613d404296b730cf9ef872541662
pawelsawicz <pawel.sawicz@justgiving.com>
2014-09-14 00:24:24 +0100

    added repository project

---
+++ LearnPledgeChallenge.WP8.Data/PledgeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnPledgeChallenge.WP8.Data
+{
+    public class PledgeRepository
+    {
+        public void Add()
+        {
+            
+        }
+    }
+
+    public class Pledge
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PledgeText { get; set; }
+        public string Forfeit { get; set; }
+        public DateTime ExpirationDateTime { get; set; }
+    }
+}
