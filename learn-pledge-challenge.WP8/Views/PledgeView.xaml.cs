9c3f5f74848c1e2e060971e88aee9bdd8fbff23f
pawelsawicz <pawel.sawicz@justgiving.com>
2014-09-13 18:15:55 +0100

    add simple mock up of views

---
+++ learn-pledge-challenge.WP8/Views/PledgeView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace learn_pledge_challenge.WP8.Views
+{
+    public partial class PledgeView : PhoneApplicationPage
+    {
+        public PledgeView()
+        {
+            InitializeComponent();
+        }
+
+        private void Pledge_OnTap(object sender, GestureEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
