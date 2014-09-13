b9cf29a259508a6a2a75ea6cb5496735317b5268
pawelsawicz <pawel.sawicz@justgiving.com>
2014-09-14 00:49:14 +0100

    challenge view changed, added view model

---
+++ LearnPledgeChallenge.WP8/ViewModels/ChallengeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace LearnPledgeChallenge.WP8.ViewModels
+{
+    public class ChallengeViewModel : PropertyChangedBase
+    {
+        private readonly INavigationService _navigationService;
+
+        public ChallengeViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public void TryChallenge()
+        {
+            MessageBox.Show("Challenge friends");
+        }
+    }
+}
