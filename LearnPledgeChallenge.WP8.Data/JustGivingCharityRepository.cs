c3fd23ca08e30615635ea41c738d1d6bdf4e8325
pawelsawicz <pawel.sawicz@justgiving.com>
2014-09-14 02:09:07 +0100

    justgiving charity repository has been created, restsharpextenstion has been creted

---
+++ LearnPledgeChallenge.WP8.Data/JustGivingCharityRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace LearnPledgeChallenge.WP8.Data
+{
+    public class JustGivingCharityRepository
+    {
+        private readonly RestClient _restClient;
+        private readonly List<int> _recommendedCharities = new List<int>(3)
+        {
+            2344,
+            11490,
+            18570
+        };
+
+        public JustGivingCharityRepository()
+        {
+            _restClient = new RestClient();
+        }
+
+        public List<Chairty> GetRecommendedChairties()
+        {
+            var listOfResult = new List<Chairty>();
+
+            foreach (var charityId in _recommendedCharities)
+            {
+                _restClient.BaseUrl = string.Format("https://api-sandbox.justgiving.com/0f938d22/v1/charity/{0}", charityId);
+                var restRequest = new RestRequest();
+                var restResponse = _restClient.ExecuteAsync(restRequest, (response, handler) =>
+                {
+                    if (response.ResponseStatus == ResponseStatus.Completed)
+                    {
+
+                    }
+                });
+            }
+            return listOfResult;
+        }
+
+        public List<Chairty> SearchForCharities(string query)
+        {
+            return new List<Chairty>();
+        }
+    }
+
+    public class Chairty
+    {
+        
+    }
+}
---
+++ LearnPledgeChallenge.WP8.Data/RestSharpExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace LearnPledgeChallenge.WP8.Data
+{
+    public static class RestSharpExtension
+    {
+        private static Task<T> ExecuteAsyncTask<T>(this RestClient restClient, IRestRequest request, Func<IRestResponse, T> selector)
+        {
+            var task = new TaskCompletionSource<T>();
+            var response = restClient.ExecuteAsync(request, r =>
+            {
+                if (r.ErrorException == null)
+                {
+                    task.SetResult(selector(r));
+                }
+                else
+                {
+                    task.SetException(r.ErrorException);
+                }
+            });
+
+            return task.Task;
+        }
+
+        public static Task<string> GetContentAsync(this RestClient restClient, IRestRequest request)
+        {
+            return restClient.ExecuteAsyncTask(request, r => r.Content);
+        }
+
+        public static Task<IRestResponse> GetResponseAsync(this RestClient restClient, IRestRequest request)
+        {
+            return restClient.ExecuteAsyncTask(request, r => r);
+        }
+    }
+}
